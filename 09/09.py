b6dfdf2b185c23f28a124765ad39523511d0de81
Kristian Brandser <kristianbrandser@kristians-mbp.lan>
2020-04-09 13:35:43 +0200

    Løsning luke 09

--- 09/09.py
+++ 09/09.py
@@ -1,2 +1,9 @@
-import math
- 
+import numpy as np
+
+Numbers = np.genfromtxt("09/numbers.txt", dtype=int, delimiter=',')
+sNumbers = np.sum(Numbers)
+
+sAllNumbers = ((Numbers.size+1)*(Numbers.size+2))/2 # 100000*100001/2
+missingNumber = sAllNumbers - sNumbers
+print("Tallet som mangler er:", missingNumber)
+
